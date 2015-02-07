84bbc41d06f7bb8cfab1a4e5937fec05e9dafed2
Chad Sikorra <Chad.Sikorra@gmail.com>
2015-02-07 17:22:45 -0600

    Add a parameter resolver. This will initially be used in the LDAP object creator.

---
+++ spec/LdapTools/ParameterResolverSpec.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * This file is part of the LdapTools package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\LdapTools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ParameterResolverSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $attributes = [
+            'firstName' => '%foo%',
+            'lastName' => '%bar%',
+            'displayName' => '%lastname%, %firstname%',
+        ];
+        $parameters = [
+            'foo' => 'Emmett',
+            'bar' => 'Brown',
+        ];
+        $this->beConstructedWith($attributes, $parameters);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('LdapTools\ParameterResolver');
+    }
+
+    function it_should_return_an_array_when_calling_resolve()
+    {
+        $this->resolve()->shouldBeArray();
+    }
+
+    function it_should_correctly_parse_passed_parameters()
+    {
+        $this->resolve()->shouldHaveKeyWithValue('firstName', 'Emmett');
+        $this->resolve()->shouldHaveKeyWithValue('lastName', 'Brown');
+        $this->resolve()->shouldHaveKeyWithValue('displayName', 'Brown, Emmett');
+    }
+
+    function it_should_be_case_insensitive_for_parameter_names()
+    {
+        $attributes = [
+            'firstName' => '%BAR%',
+            'lastName' => '%fIrStNaMe%',
+        ];
+        $parameters = [
+            'bAr' => 'Emmett'
+        ];
+        $this->beConstructedWith($attributes, $parameters);
+
+        $this->resolve()->shouldHaveKeyWithValue('firstName', 'Emmett');
+        $this->resolve()->shouldHaveKeyWithValue('lastName', 'Emmett');
+    }
+
+    function it_should_return_attributes_in_the_same_case_they_were_passed()
+    {
+        $attributes = [
+            'FirstNamE' => '%BAR%',
+        ];
+        $parameters = [
+            'bAr' => 'Emmett'
+        ];
+        $this->beConstructedWith($attributes, $parameters);
+
+        $this->resolve()->shouldHaveKey('FirstNamE');
+    }
+
+    function it_should_detect_circular_references_in_parameters()
+    {
+        $attributes = [
+            'firstName' => '%lastName%',
+            'lastName' => '%firstName%',
+            'username' => 'foo',
+        ];
+        $this->beConstructedWith($attributes, []);
+
+        $this->shouldThrow('\LogicException')->duringResolve();
+    }
+
+    function it_should_return_attributes_correctly_when_no_parameters_are_used()
+    {
+        $attributes = [
+            'firstName' => 'Foo',
+            'lastName' => 'Bar',
+        ];
+        $this->beConstructedWith($attributes, []);
+
+        $this->resolve()->shouldBeEqualTo($attributes);
+    }
+
+    function it_should_resolve_parameters_in_the_correct_order()
+    {
+        $attributes = [
+            'displayName' => '%lastname%, %firstname% as %username%',
+            'firstName' => '%foo%',
+            'lastName' => '%bar%',
+            'username' => '%city%',
+            'city' => 'Hill Valley',
+        ];
+        $parameters = [
+            'foo' => 'Emmett',
+            'bar' => 'Brown',
+        ];
+        $this->beConstructedWith($attributes, $parameters);
+
+        $this->resolve()->shouldHaveKeyWithValue('displayName', 'Brown, Emmett as Hill Valley');
+        $this->resolve()->shouldHaveKeyWithValue('firstName', 'Emmett');
+        $this->resolve()->shouldHaveKeyWithValue('lastName', 'Brown');
+        $this->resolve()->shouldHaveKeyWithValue('username', 'Hill Valley');
+        $this->resolve()->shouldHaveKeyWithValue('city', 'Hill Valley');
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'haveKeyWithValue' => function($subject, $key, $value) {
+                return isset($subject[$key]) && ($subject[$key] === $value);
+            },
+        ];
+    }
+}
---
+++ src/LdapTools/ParameterResolver.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * This file is part of the LdapTools package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LdapTools;
+
+/**
+ * Iterates over the attributes to process and replace parameter values in the required order based on dependencies.
+ *
+ * @author Chad Sikorra <Chad.Sikorra@gmail.com>
+ */
+class ParameterResolver
+{
+    /**
+     * The marker that goes on each end of a parameter.
+     */
+    const PARAM_MARKER = '%';
+
+    /**
+     * @var array All of the attributes to iterate through.
+     */
+    protected $attributes = [];
+
+    /**
+     * @var array Any explicitly set parameters.
+     */
+    protected $parameters = [];
+
+    /**
+     * @var array Contains the attribute name with an array of parameters it requires.
+     */
+    protected $requirements = [];
+
+    /**
+     * @var array Parameters that have been resolved to their final values.
+     */
+    protected $resolvedParameters = [];
+
+    /**
+     * @param array $attributes
+     * @param array $parameters
+     */
+    public function __construct(array $attributes, array $parameters)
+    {
+        $this->attributes = $attributes;
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Gets the attributes with all of the parameters resolved.
+     *
+     * @return array
+     */
+    public function resolve()
+    {
+        if (!empty($this->resolvedParameters)) {
+            return $this->attributes;
+        }
+        $this->evaluateRequirements($this->attributes);
+        $this->resolveAllParameters();
+
+        return $this->attributes;
+    }
+
+    /**
+     * Iterates over each requirement to resolve the parameters it contains.
+     */
+    protected function resolveAllParameters()
+    {
+        foreach ($this->requirements as $attribute => $parameters) {
+            // This parameter may have already been resolved as part of a dependency
+            if (!in_array($attribute, $this->resolvedParameters)) {
+                $this->resolveParametersForAttribute($attribute, $parameters);
+            }
+        }
+    }
+
+    /**
+     * Given a specific attribute with parameters, this will resolve it while first resolving any dependent attributes
+     * that first need to be resolved.
+     *
+     * @param $attribute
+     * @param array $parameters
+     */
+    protected function resolveParametersForAttribute($attribute, array $parameters)
+    {
+        $children = $this->getDependentAttributes($parameters);
+
+        if (!empty($children)) {
+            foreach ($children as $child) {
+                $this->resolveParametersForAttribute($child, $this->requirements[$child]);
+            }
+        }
+
+        $this->doResolveParametersForAttribute($attribute, $parameters);
+        $remainingParameters = $this->getParametersInValue($this->attributes[$attribute]);
+
+        // A second pass may be required for attributes it depended on that contained parameters not based on attributes
+        if (!empty($remainingParameters)) {
+            $this->doResolveParametersForAttribute($attribute, $remainingParameters);
+        }
+
+        $this->resolvedParameters[] = $attribute;
+    }
+
+    /**
+     * Sets the value for an attribute with the specified parameters.
+     *
+     * @param string $attribute
+     * @param array $parameters
+     */
+    protected function doResolveParametersForAttribute($attribute, $parameters)
+    {
+        $this->attributes[$attribute] = $this->getValueForParameters(
+            $parameters,
+            $this->attributes[$attribute],
+            $this->attributes
+        );
+    }
+
+    /**
+     * Checks for required parameter attributes that depend on other parameter attributes. Returns an array of
+     * of dependencies.
+     *
+     * @param array $parameters
+     * @return array
+     */
+    protected function getDependentAttributes($parameters)
+    {
+        $dependencies = [];
+
+        foreach ($parameters as $parameter){
+            if (isset($this->requirements[$parameter]) && !in_array($parameter, $this->resolvedParameters)) {
+                $dependencies[] = $parameter;
+            }
+        }
+
+        return $dependencies;
+    }
+
+    /**
+     * Check all attributes for what parameters they require. This populates the '$this->requirements' array. It will
+     * then iterate back over the requirements to check for issues.
+     *
+     * @param array $attributes
+     */
+    protected function evaluateRequirements(array $attributes)
+    {
+        foreach ($attributes as $attribute => $value) {
+            $value = is_array($value) ? $value : [$value];
+            $this->setRequirementsForAttribute($attribute, $value);
+        }
+
+        foreach ($this->requirements as $attribute => $parameters) {
+            $this->checkForCircularDependency($attribute, $parameters);
+        }
+    }
+
+    /**
+     * Given an attribute, set what parameters it requires.
+     *
+     * @param string $attribute
+     * @param array $value
+     */
+    protected function setRequirementsForAttribute($attribute, array $value)
+    {
+        $parameters = [];
+
+        foreach ($value as $attrValue) {
+            $parameters = array_filter(array_merge(
+                $parameters,
+                $this->getParametersInValue($attrValue)
+            ));
+        }
+
+        if (!empty($parameters)) {
+            $this->requirements[$attribute] = $parameters;
+        }
+    }
+
+    /**
+     * Given an attribute value get all parameters it expects.
+     *
+     * @param string $value
+     * @return array
+     */
+    protected function getParametersInValue($value)
+    {
+        $parameters = [];
+        $regex = '/'.self::PARAM_MARKER.'(.*?)'.self::PARAM_MARKER.'/';
+
+        if (preg_match_all($regex, $value, $matches) && isset($matches[1])) {
+            $parameters = $matches[1];
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * Given an attribute with parameter dependencies, check if any of them will become circular.
+     *
+     * @param string $parent
+     * @param array $parentParameters
+     */
+    protected function checkForCircularDependency($parent, array $parentParameters)
+    {
+        foreach ($this->requirements as $attribute => $parameters) {
+            if (in_array($parent, $parameters) && in_array($attribute, $parentParameters)) {
+                throw new \LogicException(sprintf(
+                    'Circular parameter dependency detected. Parameters "%s" and "%s" depend on each other.',
+                    $parent,
+                    $attribute
+                ));
+            }
+        }
+    }
+
+    /**
+     * Takes all parameters (%username%, %someParameter%) within an attribute value and first checks for explicitly
+     * set values for the parameter, then checks to see if the parameter name is a different attribute. If found it
+     * takes the value either explicitly set or of the other attribute and replaces it within the original attribute.
+     *
+     * @param array $parameters All of the parameters found within the value.
+     * @param string $original The original value for the attribute, containing the parameters.
+     * @param array $attributes  All of the attributes being sent to LDAP.
+     * @return string The attribute value after the passed parameters have been set.
+     */
+    protected function getValueForParameters(array $parameters, $original, array $attributes)
+    {
+        foreach ($parameters as $parameter) {
+            $value = '';
+            // Explicitly set parameters values will take precedence
+            if (array_key_exists(strtolower($parameter), array_change_key_case($this->parameters))) {
+                $value = array_change_key_case($this->parameters)[strtolower($parameter)];
+            } elseif (array_key_exists(strtolower($parameter), array_change_key_case($attributes))) {
+                $value = array_change_key_case($attributes)[strtolower($parameter)];
+            }
+            $original = preg_replace("/%$parameter%/", $value, $original);
+        }
+
+        return $original;
+    }
+}
