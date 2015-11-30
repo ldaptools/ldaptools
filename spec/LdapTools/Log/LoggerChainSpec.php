11283a226c1136d690defdf0e8ce1ba07fa0d35c
Chad Sikorra <Chad.Sikorra@gmail.com>
2015-11-29 22:36:27 -0600

    Attempt to fix travis test on PHP7.

--- spec/LdapTools/Log/LoggerChainSpec.php
+++ spec/LdapTools/Log/LoggerChainSpec.php
@@ -46,11 +46,17 @@ class LoggerChainSpec extends ObjectBehavior
 // A rather hacky way to verify what is being done. Not sure how else to configure this at the moment.
 class LoggerTest1 implements LdapLoggerInterface
 {
+    /**
+     * @param LogOperation $operation
+     */
     public function start(\LdapTools\Log\LogOperation $operation)
     {
         throw new \InvalidArgumentException("Start=".$operation->getOperation()->getDn());
     }
 
+    /**
+     * @param LogOperation $operation
+     */
     public function end(\LdapTools\Log\LogOperation $operation)
     {
         throw new \InvalidArgumentException("End=".$operation->getOperation()->getDn());
