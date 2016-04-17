ba2aa303ace1784e67e55c950f1331372f09d057
Chad Sikorra <Chad.Sikorra@gmail.com>
2016-04-17 09:54:21 -0500

    Add missing exception specs.

---
+++ spec/LdapTools/Exception/LdifParserExceptionSpec.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of the LdapTools package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\LdapTools\Exception;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class LdifParserExceptionSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith('foo', 'foo: bar', 1);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('LdapTools\Exception\LdifParserException');
+    }
+
+
+    function it_should_have_the_base_exception_type()
+    {
+        $this->shouldHaveType('\LdapTools\Exception\Exception');
+    }
+
+    function it_should_generate_a_message()
+    {
+        $this->getMessage()->shouldBeEqualTo('foo on line number 1 near "foo: bar"');
+    }
+}
