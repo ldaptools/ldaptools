e614adcd4da0283d8f4e968a2d7a4af4cdae97cf
Chad Sikorra <Chad.Sikorra@gmail.com>
2016-07-28 07:56:28 -0500

    Add an object to represent GPO links.

---
+++ spec/LdapTools/Utilities/GPOLinkSpec.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * This file is part of the LdapTools package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\LdapTools\Utilities;
+
+use LdapTools\Utilities\GPOLink;
+use PhpSpec\ObjectBehavior;
+
+class GPOLinkSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith('foo');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('LdapTools\Utilities\GPOLink');
+    }
+
+    function it_should_be_constructed_with_the_name_and_a_default_options_flag_of_zero()
+    {
+        $this->getGpo()->shouldEqual('foo');
+        $this->getOptionsFlag()->shouldBeEqualTo(0);
+    }
+
+    function it_should_be_constructed_with_an_options_flag_if_specified()
+    {
+        $this->beConstructedWith('foo', 1);
+        $this->getOptionsFlag()->shouldBeEqualTo(1);
+    }
+
+    function it_should_only_allow_options_flags_between_zero_and_three()
+    {
+        $this->shouldNotThrow('\Exception')->duringSetOptionsFlag(0);
+        $this->shouldNotThrow('\Exception')->duringSetOptionsFlag(1);
+        $this->shouldNotThrow('\Exception')->duringSetOptionsFlag(2);
+        $this->shouldNotThrow('\Exception')->duringSetOptionsFlag(3);
+        $this->shouldThrow('LdapTools\Exception\InvalidArgumentException')->duringSetOptionsFlag(4);
+    }
+
+    function it_should_set_and_get_the_gpo()
+    {
+        $this->setGpo('foobar')->shouldReturnAnInstanceOf('LdapTools\Utilities\GPOLink');
+        $this->getGpo()->shouldEqual('foobar');
+    }
+
+    function it_should_set_and_get_the_options_flag()
+    {
+        $this->setOptionsFlag(2)->shouldReturnAnInstanceOf('LdapTools\Utilities\GPOLink');
+        $this->getOptionsFlag()->shouldEqual(2);
+    }
+
+    function it_should_set_and_get_whether_the_GPO_is_enabled()
+    {
+        $this->getIsEnabled()->shouldEqual(true);
+        $this->setIsEnabled(false)->shouldReturnAnInstanceOf('LdapTools\Utilities\GPOLink');
+        $this->getIsEnabled()->shouldEqual(false);
+        $this->getOptionsFlag()->shouldBeEqualTo(GPOLink::FLAGS['IGNORED']);
+    }
+
+    function it_should_set_and_get_whether_the_GPO_is_enforced()
+    {
+        $this->getIsEnforced()->shouldEqual(false);
+        $this->setIsEnforced(true)->shouldReturnAnInstanceOf('LdapTools\Utilities\GPOLink');
+        $this->getIsEnforced()->shouldEqual(true);
+        $this->getOptionsFlag()->shouldBeEqualTo(GPOLink::FLAGS['ENFORCED']);
+    }
+
+    function it_should_allow_a_GPO_to_be_both_not_enabled_and_enforced()
+    {
+        $this->setIsEnforced(true);
+        $this->setIsEnabled(false);
+
+        $this->getIsEnforced()->shouldBe(true);
+        $this->getIsEnabled()->shouldBe(false);
+        $this->getOptionsFlag()->shouldEqual(GPOLink::FLAGS['IGNORED_ENFORCED']);
+    }
+
+    function it_should_not_modify_an_option_that_is_already_set()
+    {
+        $this->setIsEnabled(true);
+        $this->getIsEnabled()->shouldEqual(true);
+        $this->getOptionsFlag()->shouldBeEqualTo(0);
+
+        $this->getIsEnforced()->shouldEqual(false);
+        $this->setIsEnforced(false);
+        $this->getIsEnforced()->shouldEqual(false);
+
+        $this->setIsEnforced(true);
+        $this->setIsEnforced(true);
+        $this->getIsEnforced()->shouldEqual(true);
+
+    }
+
+    function it_should_have_a_string_representation_using_the_GPO_name()
+    {
+        $this->__toString()->shouldBeEqualTo('foo');
+    }
+}
---
+++ src/LdapTools/Utilities/GPOLink.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * This file is part of the LdapTools package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LdapTools\Utilities;
+
+use LdapTools\Exception\InvalidArgumentException;
+
+/**
+ * Represents a GPO link in AD.
+ *
+ * @author Chad Sikorra <Chad.Sikorra@gmail.com>
+ */
+class GPOLink
+{
+    /**
+     * These are the flag values that control whether the link is ignored/enforced.
+     */
+    const FLAGS = [
+        'NOT_IGNORED_NOT_ENFORCED' => 0,
+        'IGNORED' => 1,
+        'ENFORCED' => 2,
+        'IGNORED_ENFORCED' => 3,
+    ];
+
+    /**
+     * @var int The GPO options bit flags
+     */
+    protected $options;
+
+    /**
+     * @var string The GPO (name, DN, SID, or GUID)
+     */
+    protected $gpo;
+
+    /**
+     * @param string $gpo The GPO (name, DN, SID, or GUID)
+     * @param int $options The GPO options bit flags
+     */
+    public function __construct($gpo, $options = self::FLAGS['NOT_IGNORED_NOT_ENFORCED'])
+    {
+        $this->gpo = $gpo;
+        $this->setOptionsFlag($options);
+    }
+
+    /**
+     * Set the GPO for the link.
+     *
+     * @param string $gpo
+     * @return $this
+     */
+    public function setGpo($gpo)
+    {
+        $this->gpo = $gpo;
+
+        return $this;
+    }
+
+    /**
+     * Get the GPO for the link.
+     *
+     * @return string
+     */
+    public function getGpo()
+    {
+        return $this->gpo;
+    }
+
+    /**
+     * Set whether the GPO link is enabled.
+     *
+     * @param bool $isEnabled
+     * @return GPOLink
+     */
+    public function setIsEnabled($isEnabled)
+    {
+        return $this->modifyOptions($isEnabled, self::FLAGS['IGNORED']);
+    }
+
+    /**
+     * Get whether the GPO link is enabled.
+     *
+     * @return bool
+     */
+    public function getIsEnabled()
+    {
+        return $this->isOptionFlagSet(self::FLAGS['IGNORED']);
+    }
+
+    /**
+     * Set whether the GPO is enforced.
+     *
+     * @param bool $isEnforced
+     * @return GPOLink
+     */
+    public function setIsEnforced($isEnforced)
+    {
+        return $this->modifyOptions($isEnforced, self::FLAGS['ENFORCED']);
+    }
+
+    /**
+     * Get whether the GPO is enforced.
+     *
+     * @return bool
+     */
+    public function getIsEnforced()
+    {
+        return $this->isOptionFlagSet(self::FLAGS['ENFORCED']);
+    }
+
+    /**
+     * Set the GPO options bit flags.
+     *
+     * @param int $options
+     * @return $this
+     */
+    public function setOptionsFlag($options)
+    {
+        if (in_array($options, self::FLAGS) === false) {
+            throw new InvalidArgumentException(sprintf("The GPO options flag must be a value 0 - 3, got: %s", $options));
+        }
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
+     * Get the GPO options bit flags.
+     *
+     * @return int
+     */
+    public function getOptionsFlag()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->gpo;
+    }
+
+    /**
+     * Modifies a specific bit in the options flag. Ignores it if it is already set to the desired action.
+     *
+     * @param bool $action
+     * @param int $int
+     * @return $this
+     */
+    protected function modifyOptions($action, $int)
+    {
+        if ($this->isOptionFlagSet($int) === $action) {
+            return $this;
+        }
+
+        if ($action) {
+            $this->options = (int) $this->options | (int) $int;
+        } else {
+            $this->options = (int) $this->options ^ (int) $int;
+        }
+
+        return $this;
+    }
+
+    /**
+     * A quick check to determine if an option flag is already set.
+     *
+     * @param int $flag
+     * @return bool
+     */
+    protected function isOptionFlagSet($flag)
+    {
+        $isFlagSet = (bool) ((int) $this->options & (int) $flag);
+
+        return $flag == self::FLAGS['IGNORED'] ? !$isFlagSet : $isFlagSet;
+    }
+}
